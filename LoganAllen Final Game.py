a7be4308c6acbf330a0981e5a9421c1599a0b834
Logan1allen <logan.allen@bsu.edu>
2024-12-10 16:09:37 -0500

    Buttons
    Added Buttons to the screen, currently do nothing.

--- LoganAllen Final Game.py
+++ LoganAllen Final Game.py
@@ -8,10 +8,35 @@ class Game(simpleGE.Scene):
         self.setImage("brick.png")
         backgroundnoise = simpleGE.Sound("crickets.MP3")
         backgroundnoise.play()
+        
+        self.btnLeft = simpleGE.Button()
+        self.btnLeft.text = "Go Left"
+        self.btnLeft.center = (100,400)
+        
+        self.btnRight = simpleGE.Button()
+        self.btnRight.text = "Go Right"
+        self.btnRight.center = (540,400)
+        
+        self.btnLleft = simpleGE.Button()
+        self.btnLleft.text = "Listen"
+        self.btnLleft.center = (100, 450)
+        
+        self.btnLright = simpleGE.Button()
+        self.btnLright.text = "Listen"
+        self.btnLright.center = (540, 450)
+        
         self.sprites = [
-            
+            self.btnLeft,
+            self.btnRight,
+            self.btnLleft,
+            self.btnLright
             ]
         
+
+        
+        
+#class instructions(simpleGE.Scene):
+    
         
 def main():
     
